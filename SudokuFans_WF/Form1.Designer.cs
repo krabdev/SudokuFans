8e3f1a07addf739b828def5ae68cbf21e81e7633
Andreas Oxinos <krabdev@icloud.com>
2013-05-11 01:18:21 +0300

    Project Created
    a

---
+++ SudokuFans_WF/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SudokuFans_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(130, 105);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 24);
+            this.radButton1.TabIndex = 0;
+            this.radButton1.Text = "radButton1";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 622);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SudokuFans_WF/Logic/Initial.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuFans_WF.Logic
+{
+    class Initial
+    {
+        private int[,] _array = new int[9, 9];
+
+        public Initial()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+
+                    int[] array = new int[9];
+                    for (int x = 0; x < 9; x++)
+                    {
+                        int flag;
+                        do
+                        {
+
+                            flag = 1;
+
+                            int temp = random.Next(1, 10);
+
+                            array[x] = temp;
+                            for (int k = 0; k < x; k++)
+                            {
+                                
+                                if (temp == array[k])
+                                {
+                                    flag = 0;
+                                    break;
+                                }
+                            }
+                        } while (flag == 0);
+                    }
+
+                    int[] arrayx = new int[9]; arrayx = array;
+                    int[] arrayy = new int[9]; arrayy = array;
+                    int[] arrayz = new int[9]; arrayz = array;
+
+                    //xtest
+
+                    for (int k = 0; k < 9; k++)
+                        for (int p = 0; p < y; p++)
+                            if (arrayx[k] == _array[i, p])
+                                arrayx[k] = 0;
+
+                    //ytest
+
+                    for (int k = 0; k < 9; k++)
+                        for (int p = 0; p < i; p++)
+                            if (arrayy[k] == _array[p, y])
+                                arrayy[k] = 0;
+
+                    //btest
+
+                    {
+                        int ii = i;
+                        int yy = y;
+                        ii = ii - (ii % 3);
+                        yy = yy - (yy % 3);
+
+                        for (int k = 0; k < 9; k++)
+                            for (int p = ii; p < (ii + 3 - (i % 3)); p++)
+                            {
+                                for (int z = yy; z < (yy + 3 - (y % 3)); z++)
+                                {
+                                    if ((z >= y) && (p >= i)) break;
+
+                                    if (arrayz[k] == _array[p, z])
+                                        arrayz[k] = 0;
+                                   
+                                }
+                                
+                            }
+                    }
+                    for (int k = 0; k < 9; k++)
+                        if ((arrayz[k] == arrayx[k]) && (arrayz[k] == arrayy[k]) && (arrayx[k] == arrayy[k]) && (arrayx[k] != 0))
+                        {
+                            _array[i, y] = array[k];
+                            break;
+                        }
+                }
+            }
+            print(_array);
+        }
+
+        private int[] test1(int[] array)
+        {
+            return array;
+        }
+
+        private int[] test2(int[] array)
+        {
+            return array;
+        }
+
+        private int[] test3(int[] array)
+        {
+            return array;
+        }
+
+        public void print(int[,] a)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    Console.Write(" "); Console.Write(a[i,x].ToString());
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
